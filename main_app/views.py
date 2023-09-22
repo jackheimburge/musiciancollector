2c85062210b0aa7a31f5b19f20358ae9838277be
centipedejam <jheimburge@gmail.com>
2023-09-22 13:57:47 -0700

    First Commit

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+musicians = [
+  {'name': 'Thom Yorke', 'age': '54', 'role': 'vocalist'},
+  {'name': 'Kevin Parker', 'age': '31', 'role': 'songwriter'},
+]
+
+# Create your views here.
