0cc5d5139fa81d277491d4fcb45e16d60bf64e2e
centipedejam <jheimburge@gmail.com>
2023-09-22 14:58:18 -0700

    lab pt. 1 styling

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 musicians = [
   {'name': 'Thom Yorke', 'age': '54', 'role': 'vocalist, songwriter', 'description': 'The main vocalist and songwriter of the rock band Radiohead.'},
-  {'name': 'Kevin Parker', 'age': '31', 'role': 'songwriter, multi-instrumentalist', 'description': 'An Australian singer, songwriter, multi-instrumentalist, musician, record producer, and DJ, best known for his musical project Tame Impala.'},
+  {'name': 'Kevin Parker', 'age': '37', 'role': 'songwriter, multi-instrumentalist', 'description': 'An Australian singer, songwriter, multi-instrumentalist, musician, record producer, and DJ, best known for his musical project Tame Impala.'},
+  {'name': 'Lana Del Rey', 'age': '38', 'role': 'singer/songwriter', 'description': 'an American singer-songwriter. Her music is noted for its cinematic quality and exploration of tragic romance, glamour, and melancholia, with frequent references to contemporary pop culture and 1950s–1970s Americana.'},
 ]
 
 # Create your views here.
